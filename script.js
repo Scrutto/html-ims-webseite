1d8bf5c993ba056afccaa84ae9f2aab2c7847d9a
Jasmine Priyatna <jsmnprytn@gmail.com>
2026-06-10 10:09:47 +0200

    Kontaktformular

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('ContactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('formName').value;
+    const email = document.getElementById('formEmail').value;
+    const subject = document.getElementById('formSubject').value;
+    const message = document.getElementById('formMessage').value;
+
+    const emailTo = "jsmnprytn@gmail.com";
+    const emailSubject = encodeURIComponent(subject || "Kontaktformular Nachricht");
+    
+    const emailBody = encodeURIComponent(
+      "Name: " + name + "\n" +
+      "Kontakt: " + email + "\n\n" +
+      "Nachricht:\n" + message
+    );
+
+    window.location.href = "mailto:" + emailTo + "?subject=" + emailSubject + "&body=" + emailBody;
+
+    document.getElementById('ContactForm').style.display = 'none';
+    document.getElementById('successState').style.display = 'block';
+});
